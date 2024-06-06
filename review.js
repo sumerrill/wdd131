5033aa14ca5b14811c84cae7261529c3824ccc20
su <ssu65708@gmail.com>
2024-06-05 22:59:11 -0700

    review.html added

---
+++ review.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const currentYear = new Date().getFullYear();
+    const lastModifiedDate = new Date(document.lastModified);
+    const formattedDate = lastModifiedDate.toLocaleString('en-US', { dateStyle: 'short', timeStyle: 'medium' });
+
+    document.getElementById('year').textContent = currentYear;
+    document.getElementById('lastModified').textContent = formattedDate;
+});
